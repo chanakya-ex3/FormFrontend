3dbd6a55b312c52654e544e588ab0369318ee7b7
Chanakya BM <chanakya.exe@gmail.com>
2025-04-25 19:57:25 +0530

    updated button text 2

--- src/Components/FormComponent/Form.jsx
+++ src/Components/FormComponent/Form.jsx
@@ -251,7 +251,7 @@ const DynamicForm = ({ placemarks, position }) => {
             ) : (
               <button
                 onClick={goToNextPage}
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="bg-blue-500  px-4 py-2 rounded"
               >
                 Next
               </button>
