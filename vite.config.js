96dca41b34da8729acfb9d19838403b7ef4dae5d
Chanakya BM <chanakya.exe@gmail.com>
2025-04-25 19:19:46 +0530

    added webapp configs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import pwa from 'vite-plugin-pwa' // Import the PWA plugin
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    pwa({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App Short Name',
+        description: 'A description of your app',
+        theme_color: '#ffffff', // Background color
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 })
