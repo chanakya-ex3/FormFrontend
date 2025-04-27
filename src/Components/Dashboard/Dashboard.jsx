d70f383a9a9b54728f2d03154f46e16aebdca10a
Chanakya BM <chanakya.exe@gmail.com>
2025-04-28 02:51:26 +0530

    test 2

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -32,7 +32,7 @@ const Dashboard = () => {
                 <div>
                     {/* App Bar */}
                     <div className="fixed top-0 left-0 right-0 p-4 bg-violet-400 shadow-md flex justify-between items-center z-50">
-                        <p className=" font-bold text-white">Form App</p>
+                        <p className="font-bold text-white">Form App</p>
                         <button onClick={logout} className="bg-red-500 hover:bg-red-600 p-2 rounded text-white">
                             Logout
                         </button>
